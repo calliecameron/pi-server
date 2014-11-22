730eaa3b908ca7ea99cc4cf7664937c3df91dacf
Callum Cameron <cjcameron7@gmail.com>
2014-11-22 14:23:39 +0000

    Testing setup.

---
+++ zz-vm-testing/fakeInternet-00-setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+sudo apt-get update &&
+sudo apt-get upgrade &&
+sudo apt-get dist-upgrade
+
+if ! grep eth1 /etc/network/interfaces; then
+    sudo tee -a /etc/network/interfaces &>/dev/null <<EOF
+
+# The fake internet
+allow-hotplug eth1
+iface eth1 inet dhcp
+EOF
+
+fi
+
+
+DST='/etc/network/if-up.d/fakeInternet-boot-config'
+sudo cp "${DIR}/fakeInternet-boot-config" "${DST}" &&
+sudo chown root:root "${DST}" &&
+sudo chmod a=r "${DST}" &&
+sudo chmod u+x "${DST}"
---
+++ zz-vm-testing/fakeInternet-boot-config
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sysctl -w net.ipv4.ip_forward=1
