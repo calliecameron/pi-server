005f10280c4c412e72b9f47a599eefcd2b81af3f
Callum Cameron <cjcameron7@gmail.com>
2014-11-23 14:16:59 +0000

    Config scripts.

--- 01-core-owncloud/common.bash
+++ 01-core-owncloud/common.bash
@@ -1,9 +1,22 @@
 # Common stuff; should be sourced, not run!
 
+function get-pi-server-param()
+{
+    if [ ! -z "${1}" ] && [ -e "${1}" ]; then
+        cat "${1}"
+    else
+        echo
+    fi
+}
 
-# Is this really a Pi, or is it a Debian VM for testing?
+export PI_SERVER_DIR='/etc/pi-server'
+
+# export PI_SERVER_IP_FILE
+
+
 function real-pi()
 {
+    # Is this really a Pi, or is it a Debian VM for testing?
     if [ "$(uname -m)" = 'armv6l' ]; then
         return 0
     else
@@ -14,4 +27,16 @@ function real-pi()
 function enter-to-continue()
 {
     read -p "Press ENTER to continue..."
+}
+
+function yn_y()
+{
+    # Y is the default
+    local REPLY
+    read -p "${1} [Y/n] " -r
+    if [[ $REPLY =~ ^[Nn]$ ]]; then
+        return 1
+    else
+        return 0
+    fi
 }
