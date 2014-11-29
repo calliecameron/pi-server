efceecfbd56f5c20b8dbfc9627aea221912698b9
Callum Cameron <cjcameron7@gmail.com>
2014-11-29 14:06:28 +0000

    Test network setup.

--- zz-vm-testing/fakeClient-00-setup.sh
+++ zz-vm-testing/fakeClient-00-setup.sh
@@ -4,7 +4,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 sudo apt-get update &&
 sudo apt-get -y upgrade &&
-sudo apt-get -y dist-upgrade
+sudo apt-get -y dist-upgrade &&
+sudo apt-get -y install openssh-server openvpn
 
 
 echo &&
