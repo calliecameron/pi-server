c621ac13e8543fe191507b97bd265f57f7b0d8d0
Callum Cameron <cjcameron7@gmail.com>
2014-11-29 15:37:35 +0000

    Setup scripts.

--- zz-vm-testing/fakeRouter-00-setup.sh
+++ zz-vm-testing/fakeRouter-00-setup.sh
@@ -19,6 +19,7 @@ fi
 
 read -p "Enter the address of the default route (FakeInternet): " DEFAULT_ROUTE &&
 read -p "Enter the LAN address of this router's FakePi (for port forwarding): " FAKE_PI &&
+read -p "Enter the FakePi's VPN network address, without the '/24': " PI_VPN_NET &&
 
 CONFIG_SRC="${DIR}/fakeRouter-boot-config" &&
 CONFIG_DST='/etc/network/if-up.d/fakeRouter-boot-config' &&
@@ -26,6 +27,7 @@ TMPFILE="$(mktemp)" &&
 cp "${CONFIG_SRC}" "${TMPFILE}" &&
 sed -i "s/@@@@@1@@@@@/${DEFAULT_ROUTE}/g" "${TMPFILE}" &&
 sed -i "s/@@@@@2@@@@@/${FAKE_PI}/g" "${TMPFILE}" &&
+sed -i "s/@@@@@3@@@@@/${PI_VPN_NET}/g" "${TMPFILE}" &&
 sudo cp "${TMPFILE}" "${CONFIG_DST}" &&
 sudo chown root:root "${CONFIG_DST}" &&
 sudo chmod a=r "${CONFIG_DST}" &&
