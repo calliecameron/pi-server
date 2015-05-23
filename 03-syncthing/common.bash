2bb1dc4847d6adaf7eb2db88630b1c44a3e6409e
Callum Cameron <cjcameron7@gmail.com>
2015-05-23 16:18:19 +0100

    Tweaked syncthing settings.

--- 03-syncthing/common.bash
+++ 03-syncthing/common.bash
@@ -3,6 +3,4 @@
 source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../01-core/common.bash"
 
 export PI_SERVER_SYNCTHING_BINARY="${PI_SERVER_DIR}/syncthing"
-export PI_SERVER_SYNCTHING_ROOT="${PI_SERVER_DATA_MOUNT_DIR}/syncthing-main"
-export PI_SERVER_SYNCTHING_CONFIG="${PI_SERVER_SYNCTHING_ROOT}/config"
-export PI_SERVER_SYNCTHING_DATA="${PI_SERVER_SYNCTHING_ROOT}/data"
+export PI_SERVER_SYNCTHING_CONFIG="${PI_SERVER_DATA_CONFIG_DIR}/syncthing"
