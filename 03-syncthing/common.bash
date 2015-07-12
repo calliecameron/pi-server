7235d165703bcf71269bf2fc697e39c74556a84c
Callum Cameron <cjcameron7@gmail.com>
2015-07-12 13:43:02 +0100

    Syncthing now installed through apt-get.

--- 03-syncthing/common.bash
+++ 03-syncthing/common.bash
@@ -2,5 +2,5 @@
 
 source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../01-core/common.bash"
 
-export PI_SERVER_SYNCTHING_BINARY="${PI_SERVER_DIR}/syncthing"
+export PI_SERVER_SYNCTHING_BINARY='/usr/bin/syncthing'
 export PI_SERVER_SYNCTHING_CONFIG="${PI_SERVER_DATA_CONFIG_DIR}/syncthing"
