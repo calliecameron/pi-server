15462c3d2a28c41dbe5583ae9a23abeb8c74474e
Callum Cameron <cjcameron7@gmail.com>
2016-05-11 19:27:30 +0100

    00-media-server updated.

--- old-03-owncloud/common.bash
+++
@@ -1,7 +0,0 @@
-# Common stuff; should be sourced, not run!
-
-source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../01-core/common.bash"
-
-export PI_SERVER_OWNCLOUD_DATA_PATH="${PI_SERVER_DATA_MOUNT_DIR}/owncloud-main"
-export PI_SERVER_OWNCLOUD_DB='owncloud'
-export PI_SERVER_OWNCLOUD_DB_USER='ownclouduser'
