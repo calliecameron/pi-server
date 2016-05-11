764d7a9dc05ebf2c59cf63d42b08bf031d6c2941
Callum Cameron <cjcameron7@gmail.com>
2016-05-11 16:11:10 +0100

    Syncthing.

--- zz-vm-testing/fakeClient-00-setup.sh
+++ zz-vm-testing/fakeClient-00-setup.sh
@@ -9,6 +9,7 @@ sudo apt-get -y install curl openssh-server openvpn &&
 
 curl -s https://syncthing.net/release-key.txt | sudo apt-key add - &&
 echo deb http://apt.syncthing.net/ syncthing release | sudo tee /etc/apt/sources.list.d/syncthing-release.list &&
+sudo add-apt-repository -y ppa:nilarimogard/webupd8 &&
 sudo apt-get update &&
 sudo apt-get -y install syncthing syncthing-gtk &&
 
