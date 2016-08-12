99094d2323dc5db0c4dce4984dbffae482b8c10f
Callum Cameron <cjcameron7@gmail.com>
2016-08-12 18:22:00 +0100

    Custom hostapd now breaks things; get rid of it.

--- 05-wireless-router/common.bash
+++ 05-wireless-router/common.bash
@@ -1,5 +1,3 @@
 # Common stuff; should be sourced, not run!
 
 source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../01-core/common.bash"
-
-export PI_SERVER_HOSTAPD_INSTALLED_FILE="${PI_SERVER_DIR}/hostapd-installed"
