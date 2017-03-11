f6a0e7c4c219ac72a9912214fa025d1d439739eb
Callum Cameron <cjcameron7@gmail.com>
2017-03-11 22:01:03 +0100

    Systemd.

--- 04-syncthing/common.bash
+++ 04-syncthing/common.bash
@@ -1,6 +1,6 @@
 # Common stuff; should be sourced, not run!
 
-source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../02-pi-core/common.bash"
+source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../02-pi-core/common.bash" || exit 1
 
 export PI_SERVER_SYNCTHING_BINARY='/usr/bin/syncthing'
 export PI_SERVER_SYNCTHING_CONFIG="${PI_SERVER_DATA_CONFIG_DIR}/syncthing"
