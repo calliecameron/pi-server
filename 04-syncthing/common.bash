857fc59b5e45257ac5bfe9f3ba0c5b93eb1277e7
Callum Cameron <cjcameron7@gmail.com>
2017-07-06 08:07:23 +0200

    Run syncthing using systemd.

--- 04-syncthing/common.bash
+++ 04-syncthing/common.bash
@@ -4,3 +4,4 @@ source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../02-pi-core/common.b
 
 export PI_SERVER_SYNCTHING_BINARY='/usr/bin/syncthing'
 export PI_SERVER_SYNCTHING_CONFIG="${PI_SERVER_DATA_CONFIG_DIR}/syncthing"
+export PI_SERVER_SYNCTHING_SERVICE='pi-server-syncthing.service'
