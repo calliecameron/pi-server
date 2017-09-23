dd1d6eeb057b176688318238fc3fcd32e6659d2d
Callum Cameron <cjcameron7@gmail.com>
2017-09-23 13:58:53 +0200

    Fix the OS check in 02-pi-core.

--- 02-pi-core/common.bash
+++ 02-pi-core/common.bash
@@ -2,9 +2,11 @@
 
 source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../01-generic-core/common.bash" || exit 1
 
-if ! grep stretch /etc/apt/sources.list &>/dev/null; then
-    echo-red "This only works on Raspbian/Debian Stretch; you are not on this."
-    exit 1
+if [ -z "${SKIP_OS_CHECK}" ]; then
+    if ! grep stretch /etc/apt/sources.list &>/dev/null; then
+        echo-red "This only works on Raspbian/Debian Stretch; you are not on this."
+        exit 1
+    fi
 fi
 
 
