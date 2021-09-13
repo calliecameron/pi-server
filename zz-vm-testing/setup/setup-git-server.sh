e6be3cc0071d2dc375f23a2e260970bdfdaa3da1
Callie Cameron <cjcameron7@gmail.com>
2021-09-13 09:11:16 +0200

    Update internet and routers to ubuntu 20.04.

--- zz-vm-testing/setup/setup-git-server.sh
+++ zz-vm-testing/setup/setup-git-server.sh
@@ -5,6 +5,9 @@ FOO="${REPO_ROOT}/foo"
 BAR="${REPO_ROOT}/bar"
 TEMP="${REPO_ROOT}/temp"
 
+git config --global user.name foo
+git config --global user.email foo@testbed
+
 function setup-repo() {
     REPO="${1}"
     mkdir -p "${REPO}" &&
